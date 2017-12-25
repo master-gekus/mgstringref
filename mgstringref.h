6d9a6f5c758b9fc3302f6b23b15de6f81ed83bc3
Eugeny V. Zalivochkin <master.gekus@gmail.com>
2017-12-25 19:54:47 +0300

    Implemented simple (empty) constructor and unit tests for it.

--- mgstringref.h
+++ mgstringref.h
@@ -4,10 +4,10 @@
 namespace mg {
     template<typename _CharT, typename _Traits = std::char_traits<_CharT>,
              typename _Alloc = std::allocator<_CharT> >
-    class basic_stringref
+    class basic_stringref final
     {
         typedef typename std::allocator_traits<_Alloc>::template
-            rebind<_CharT>::other _Char_alloc_type;
+            rebind_alloc<_CharT> _Char_alloc_type;
         typedef std::allocator_traits<_Char_alloc_type> _Alloc_traits;
 
     public:
@@ -16,13 +16,41 @@ namespace mg {
         typedef _Char_alloc_type                        allocator_type;
         typedef typename _Alloc_traits::size_type       size_type;
         typedef typename _Alloc_traits::difference_type difference_type;
-        typedef typename _Alloc_traits::reference       reference;
-        typedef typename _Alloc_traits::const_reference const_reference;
+        typedef value_type&                             reference;
+        typedef value_type const&                       const_reference;
         typedef typename _Alloc_traits::pointer         pointer;
         typedef typename _Alloc_traits::const_pointer   const_pointer;
+
+    private:
+        struct _Data {
+            mutable std::atomic<int> ref_;
+            size_t len_;
+            value_type const* ptr_;
+            value_type data_[] alignas(alignof(value_type));
+        };
+        static constexpr const std::size_t _Data_Header_Len
+            = (sizeof(_Data) + sizeof(value_type) - 1) / sizeof(value_type);
+
+    public:
+        explicit basic_stringref(const _Alloc& a = _Alloc()) :
+            d_(nullptr), a_(a)
+        {}
+
+        ~basic_stringref()
+        {
+            if (d_ && (0 == (--d_->ref_))) {
+                _Alloc_traits::deallocate(a_, reinterpret_cast<value_type*>(d_), d_->len_ + _Data_Header_Len);
+            }
+        }
+
+    private:
+        _Data *d_;
+        _Char_alloc_type a_;
     };
 
-    typedef basic_stringref<char>string;
+    typedef basic_stringref<char> stringref;
+    typedef basic_stringref<char16_t> ustringref;
+    typedef basic_stringref<wchar_t> wstringref;
 }
 
 
--- tests/mgstringref_test.cpp
+++ tests/mgstringref_test.cpp
@@ -58,6 +58,17 @@ namespace inplace {
             buffer_[offset] = 0;
         }
 
+        size_t used_block_count()
+        {
+            size_t count = 0;
+            for (size_t i = 0; i < block_count_; i++) {
+                if (0 != buffer_[i * block_size_]) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
     private:
         char *buffer_;
         size_t block_size_;
@@ -68,18 +79,37 @@ namespace inplace {
 
     typedef std::basic_string<char, std::char_traits<char>, allocator<char> > string;
     typedef std::basic_string<char16_t, std::char_traits<char16_t>, allocator<char16_t> > ustring;
+    typedef mg::basic_stringref<char, std::char_traits<char>, allocator<char> > stringref;
+    typedef mg::basic_stringref<char16_t, std::char_traits<char16_t>, allocator<char16_t> > ustringref;
 }
 
-TEST(Helpers, CustomAllocator)
+class CustomAllocator : public ::testing::Test
+{
+public:
+    CustomAllocator() :
+        a(buf, sizeof(buf))
+    {}
+
+protected:
+    char buf[4096 * 100];
+    ::inplace::allocator<char> a;
+
+    void TearDown() override
+    {
+        EXPECT_EQ(a.used_block_count(), static_cast<size_t>(0));
+    }
+};
+
+TEST_F(CustomAllocator, StandardContainers)
 {
     using namespace inplace;
-    char buf[4096 * 100];
-    allocator<char> a(buf, sizeof(buf));
     string *s = new string("String must be lenght enough to allocated memory.", a);
     string s1(*s);
     string s2 = *s;
+    EXPECT_EQ(a.used_block_count(), static_cast<size_t>(3));
     delete s;
     EXPECT_EQ(s1, s2);
+    EXPECT_EQ(a.used_block_count(), static_cast<size_t>(2));
 
     std::map<string, string, std::less<string>, allocator<std::map<string, string>::value_type> > m(a);
     m.emplace(string("String must be lenght enough to allocated memory.", a),
@@ -87,4 +117,18 @@ TEST(Helpers, CustomAllocator)
 
     EXPECT_EQ(m.size(), static_cast<size_t>(1));
     EXPECT_EQ(m.begin()->first, s1);
+}
+
+TEST(StandardAllocator, EmptyConstrution)
+{
+    using namespace mg;
+    stringref s;
+    ustringref us;
+}
+
+TEST_F(CustomAllocator, EmptyConstrution)
+{
+    using namespace inplace;
+    stringref s(a);
+    ustringref us(a);
 }
